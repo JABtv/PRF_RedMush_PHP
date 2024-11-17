13b61aae82685c021b15391f8d4ad06bb3beef81
JABtv <tom.andres31000@gmail.com>
2024-11-17 02:24:24 +0100

    Ajout du HEADER et FOOTER st{TODO Style.css}

--- app/views/communs/_header.php
+++ app/views/communs/_header.php
@@ -1,1 +1,50 @@
-<?php
+<!DOCTYPE html>
+<html lang="en" class="theme-light">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>RedMush</title>
+    <!-- SEO -->
+    <meta
+        name="description"
+        content="Le streetwear réinventé. Découvre notre collection de vêtements et d'accessoires streetwear pour un style urbain et affirmé. Des pièces uniques et tendances pour te démarquer au quotidien." />
+    <!-- Link Perso. -->
+    <link rel="stylesheet" href="../../public/src/style/style.css" />
+</head>
+<body>
+<header>
+    <p>FRAIS DE PORT OFFERTS À PARTIR DE 80€</p>
+    <nav class="navbar">
+      <div class="left">
+        <img
+          src="../../public/assets/images/icons/black/iconUser.png"
+          alt="User" />
+        <img
+          src="../../public/assets/images/icons/black/iconShoppingBag.png"
+          alt="shoppingBag" />
+      </div>
+      <div class="center">
+        <img
+          src="../../public/assets/images/Logo/logoGimpB.svg"
+          alt="Logo"
+        <h1>RedMush</h1>
+      </div>
+      <div class="right">
+        <form action="#" method="get">
+          <input type="text" name="searchBar">
+          <button>
+            <img
+              src="../../public/assets/images/icons/black/iconSearch.png"
+              alt="search"
+              class="iconSearch" />
+          </button>
+        </form>
+
+        </label>
+        <img
+          src="../../public/assets/images/icons/black/iconMenu.png"
+          alt="burger" />
+      </div>
+    </nav>
+</header>
+
