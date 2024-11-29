d7d28835dd69825950b58f67dbfebf39251ab235
JABtv <tom.andres31000@gmail.com>
2024-11-29 23:00:59 +0100

    MAJ index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ switch ($request_uri ) {
         include 'app/controllers/_Accueil.php';
         break;
     case '/PRF_RedMush_PHP/app/controllers/_Inscription.php':
-    case 'redmush.fr':
+    case 'redmush.fr/inscription':
         include 'app/controllers/_Inscription.php';
         default:
             include 'app/controllers/_404.php';
