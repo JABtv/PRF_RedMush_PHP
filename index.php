6e68aecf43405850a3dbcb1960fc767fc8121bbc
JABtv <tom.andres31000@gmail.com>
2024-11-30 00:15:47 +0100

    [RESTORED]Test index

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
 $request_uri = $_SERVER['REQUEST_URI'];
-$host = $_SERVER['HTTP_HOST'];
+echo "Requested URI: " . htmlspecialchars($request_uri); // Affiche l'URI demandée
 
 // Normaliser l'URI pour le switch
 switch ($request_uri) {
+
     case '/PRF_RedMush_PHP/':
-    case $host:
+    case 'redmush.fr':
         include 'app/controllers/_Accueil.php';
         break;
 
     case '/PRF_RedMush_PHP/Inscription':
-    case $host . '/Inscription':
+    case 'redmush.fr/Inscription':
         include 'app/controllers/_Inscription.php';
         break;
 
