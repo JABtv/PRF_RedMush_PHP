c385f1031df0a11a1c98e251a756652677f7c554
JABtv <tom.andres31000@gmail.com>
2024-11-29 20:47:02 +0100

    Update rooting index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $request_uri = $_SERVER['REQUEST_URI'];
 
-if ($request_uri == '/PRF_RedMush_PHP/') {
+if ($request_uri == '/PRF_RedMush_PHP/' || 'redmush.fr') {
     include 'app/controllers/_Accueil.php';
 } else {
     include 'app/views/error/_404.php'; // Page non trouvée
